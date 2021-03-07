57ea971ff271bb17ca3a1b6d16256993ca672668
BeingTiger007 <aslam.tiger.9047@gmail.com>
2021-03-07 21:44:19 +0530

    added comments and fixed some code bugs

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,38 @@
 #include <Arduino.h>
 #include "WiFi.h"
 
-#define WIFI_NETWORK "ASLAM SHAIKH 2"
-#define WIFI_PASSWORD "8855954049ert"
-#define WIFI_TIMEOUT_MS 60000
+/* Author : Aslam Shaikh
+*  Date : 07-03-2021
+*  IDE : VS Code 1.54.1
+*  Board : DOIT ESP32 DEVKIT V1 
+*/
+
+#define WIFI_NETWORK "ASLAM SHAIKH 1" // defined constant value wifi ssid
+#define WIFI_PASSWORD "8855954049vbn" // defined constant value wifi pass
+#define WIFI_TIMEOUT_MS 60000         // defined constant value 60 seconds
 
 void connectionToWiFi()
 {
-  Serial.print("Connecting to WiFi");
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi ");
+  WiFi.mode(WIFI_STA);                     // sets ESP32 operating mode as station/client
+  WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD); // passed ssid and pass and started connection
 
-  unsigned long startAttemptTime = millis();
+  unsigned long startAttemptTime = millis(); // millis() assings starting time of ESP 32
 
   while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < WIFI_TIMEOUT_MS)
   {
-    Serial.print(".");
-    delay(100);
+    Serial.print(" . "); // prints the " . " after every 100 milliseconds
+    delay(100);          // this loop ends when ESP32 connects to the wifi
   }
 
   if (WiFi.status() != WL_CONNECTED)
   {
-    Serial.println("Failed to Connect WiFi");
+    Serial.println("Failed to Connect WiFi "); // it prints the fail message on serial interface
   }
   else
   {
-    Serial.print("Connected to WiFi");
-    Serial.println(WiFi.localIP());
+    Serial.println("Connected to WiFi "); // it prints the success message on serial
+    Serial.println(WiFi.localIP());       // it prints the IP address of ESP32 on serial
   }
 }
 
